5f515455279873e25394fe81540d8370a7dfcf02
vicky <pratamavr@gmail.com>
2026-04-02 21:23:32 +0800

    updates

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -44,6 +44,7 @@ class WeatherController extends Controller
     // 1. Jika ada user dan dia punya lokasi default, gunakan itu
     if ($telegramUser && $this->userHasDefaultLocation($telegramUser)) {
       $data = $this->weatherService->getWeatherForUser($telegramUser);
+      \Log::debug("Current", ['data' => $data]);
     }
     // 2. Jika tidak, coba gunakan input manual dari request
     else {
