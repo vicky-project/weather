e86866b0a2087f5a83d9adfb194e70b168662f60
vicky <pratamavr@gmail.com>
2026-04-02 11:16:36 +0800

    updates

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -90,6 +90,7 @@ class WeatherController extends Controller
     }
 
     $data = $this->weatherService->getHourlyForecast($location);
+    \Log::debug("Forecast data.", ["forecast" => $data]);
     if (!$data) {
       return response()->json(['success' => false, 'message' => 'Data forecast tidak tersedia'], 404);
     }
