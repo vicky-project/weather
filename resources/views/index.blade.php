836be693636921d6e04bae06c3130b78177b5cab
vicky <pratamavr@gmail.com>
2026-04-11 12:44:23 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -193,10 +193,10 @@
 
   // ======================== FUNGSI UTAMA ========================
   function getWindDirection(deg) {
-  alert(deg);
   if (deg === undefined || deg === null) return '';
   const directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
   const idx = Math.round(deg / 45) % 8;
+  alert(idx);
   return directions[idx];
   }
 
