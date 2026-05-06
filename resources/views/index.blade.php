30c315e14423b43bb29cec835961a1dad5f5cbfd
vicky <pratamavr@gmail.com>
2026-05-07 03:19:37 +0800

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -594,6 +594,7 @@
   const statusSpan = document.getElementById('locationStatus');
   if (statusSpan) statusSpan.innerText = 'Meminta lokasi...';
   const tg = window.Telegram?.WebApp;
+  tg.LocationManager?.init();
   if (tg && tg.LocationManager && typeof tg.LocationManager.getLocation === 'function') {
   tg.LocationManager.getLocation((location) => {
   if (location) {
